91afba0d45da222fcced590711a45d7e97fd7149
Ronnakorn Suwanchatree <ronnakorn.suw@gmail.com>
2017-06-04 12:24:59 +0700

    bean LifeCycle

---
+++ spring-demo-one/src/com/ronnakorn/springdemo/BeanLifeCycleDemoApp.java
@@ -0,0 +1,22 @@
+package com.ronnakorn.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanLifeCycleDemoApp {
+
+	public static void main(String[] args) {
+		
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
+		
+		// retrieve bean form spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		System.out.println(theCoach.getDailyWorkout());
+		
+		// close the context
+		context.close();
+	}
+
+}
--- spring-demo-one/src/com/ronnakorn/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/ronnakorn/springdemo/TrackCoach.java
@@ -22,7 +22,17 @@ public class TrackCoach implements Coach {
 	public String getDailyFortune() {
 		return "Just Do It: " + fortuneService.getFortune();
 	}
-
+	
+	
+	// add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMyStartupStuff");
+	}
+	
+	// add a destroy method
+	public void doMyCleanupStuffYoYo() {
+		System.out.println("TrackCoach: inside method doMyCleanupStuffYoYo");
+	}
 }
 
 
