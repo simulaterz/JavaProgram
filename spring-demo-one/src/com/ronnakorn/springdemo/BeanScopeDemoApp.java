0b047bb082790b4225a9d43c1f4b5ce92f78442e
Ronnakorn Suwanchatree <ronnakorn.suw@gmail.com>
2017-06-04 11:00:40 +0700

    learning prototype scope

---
+++ spring-demo-one/src/com/ronnakorn/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,29 @@
+package com.ronnakorn.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		
+		// retrieve bean form spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+		
+		// check if they are the save
+		boolean result = (theCoach == alphaCoach);
+		
+		// print out the results
+		System.out.println("Pointing to the same object: " + result);
+		
+		System.out.println("Memory location for theCoach: " + theCoach);
+		System.out.println("Memory location for alphaCoach: " + alphaCoach);
+		
+		context.close();
+	}
+
+}
