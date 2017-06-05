0d084fa2142283503d1210b3e0ee5bcbf4b66b6a
Ronnakorn Suwanchatree <ronnakorn.suw@gmail.com>
2017-06-05 21:04:16 +0700

    Learn injection in constructor setter and field injection

--- spring-demo-annotations/src/com/ronnakorn/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/ronnakorn/springdemo/TennisCoach.java
@@ -6,12 +6,28 @@ import org.springframework.stereotype.Component;
 @Component 
 public class TennisCoach implements Coach {
 
+	@Autowired
 	private FortuneService fortuneService;
 	
+	// define a default constructor
+	public TennisCoach() {
+		System.out.println(">>TennisCoach: inside default constuctor");
+	}
+	
+	/*
+	@Autowired
+	public void doSomeCrazyStuff(FortuneService theFortuneService) {
+		System.out.println(">>TennisCoach: inside doSomeCrazyStuff() method");
+		fortuneService = theFortuneService;
+	}
+	*/
+	
+	/*
 	@Autowired
 	public TennisCoach(FortuneService theFortuneService) {
 		fortuneService = theFortuneService;
 	}
+	*/
 	
 	@Override
 	public String getDailyWorkout() {
