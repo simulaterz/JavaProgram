4e925345feb4b36e7885634731bb5effd62a3962
Ronnakorn Suwanchatree <ronnakorn.suw@gmail.com>
2017-06-19 21:12:14 +0700

    Binding Request Params

--- spring-mvc-demo/src/com/ronnakorn/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/ronnakorn/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -20,7 +21,7 @@ public class HelloWorldController {
 	}
 	
 	@RequestMapping("/processFormVersionTwo")
-	public String letShoutDude(HttpServletRequest request, Model model) {
+	public String letsShoutDude(HttpServletRequest request, Model model) {
 		
 		String theName = request.getParameter("studentName");
 		
@@ -32,5 +33,20 @@ public class HelloWorldController {
 		
 		// View to render
 		return "helloworld";
-	}	
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThreee(
+			@RequestParam("studentName") String theName,
+			Model model) {
+		
+		theName = theName.toUpperCase();
+		
+		String result = "Hey V3 ! " + theName;
+		
+		model.addAttribute("message", result);
+		
+		// View to render
+		return "helloworld";
+	}
 }
